f4264e1303460a851bf0791de56ba08b5d02e110
Oliver Searle-Barnes <searle-barneso@ukh109051.(none)>
2010-03-22 14:37:10 +0000

    Queries must now be select statements

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,5 @@
 class Report < ActiveRecord::Base
   attr_accessible :name, :query, :database_id
   belongs_to :database
+  validates_format_of :query, :with => /^select.*/, :message => 'must be a select statement'
 end
